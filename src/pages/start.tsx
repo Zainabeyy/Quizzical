3f7b8a95ac3ee8e4338e08ca75329f5e187625f3
Zainabeyy <zainabazeem324@gmail.com>
2024-07-19 14:07:35 +0500

    minor changes

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -18,43 +18,42 @@ export default function Start() {
     setCategory(e.target.value);
   }
   const [errorMessage,setErrorMessage]=React.useState("");
+
+  const fetchData = async () => {
+    let apiUrl = "";
+    if (category === "General Knowledge") {
+      apiUrl =
+        "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple";
+    } else if (category === "Books") {
+      apiUrl =
+        "https://opentdb.com/api.php?amount=5&category=10&difficulty=easy&type=multiple";
+    } else if (category === "Cartoon and Animations") {
+      apiUrl =
+        "https://opentdb.com/api.php?amount=5&category=32&difficulty=easy&type=multiple";
+    } else if (category === "Science and Nature") {
+      apiUrl =
+        "https://opentdb.com/api.php?amount=5&category=17&difficulty=easy&type=multiple";
+    }
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const shuffledData = data.results.map((quiz: QuizAPI) => {
+          const options = [...quiz.incorrect_answers, quiz.correct_answer];
+          const shuffledOptions = options.sort(() => Math.random() - 0.5);
+          return { ...quiz, shuffledOptions };
+        });
+        setQuizData(JSON.stringify(shuffledData));
+      } catch (error) {
+        console.error("Error fetching quiz data:", error);
+        setErrorMessage(`Error fetching quiz data:, ${error}, Please try again`);
+      }
+  };
   React.useEffect(() => {
     setQuizData("");
-    const fetchData = async () => {
-      let apiUrl = "";
-      if (category === "General Knowledge") {
-        apiUrl =
-          "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple";
-      } else if (category === "Books") {
-        apiUrl =
-          "https://opentdb.com/api.php?amount=5&category=10&difficulty=easy&type=multiple";
-      } else if (category === "Cartoon and Animations") {
-        apiUrl =
-          "https://opentdb.com/api.php?amount=5&category=32&difficulty=easy&type=multiple";
-      } else if (category === "Science and Nature") {
-        apiUrl =
-          "https://opentdb.com/api.php?amount=5&category=17&difficulty=easy&type=multiple";
-      }
-        try {
-          const response = await fetch(apiUrl);
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          const data = await response.json();
-          setErrorMessage("")
-          const shuffledData = data.results.map((quiz: QuizAPI) => {
-            const options = [...quiz.incorrect_answers, quiz.correct_answer];
-            const shuffledOptions = options.sort(() => Math.random() - 0.5);
-            return { ...quiz, shuffledOptions };
-          });
-          setQuizData(JSON.stringify(shuffledData));
-        } catch (error) {
-          console.error("Error fetching quiz data:", error);
-          setErrorMessage(`Error fetching quiz data:, ${error}, Please try again`);
-        }
-    };
-    let timeoutId = setTimeout(fetchData, 1000);
-    return () => clearTimeout(timeoutId);
+    fetchData()
   }, [category]);
   return (
     <div className="startPage">
@@ -62,7 +61,7 @@ export default function Start() {
       <div className="startContent">
         <h1 className="title text-center">Quizzical</h1>
         <p className="description text-center">Some description if needed</p>
-        {!errorMessage ? 
+        {quizData ? 
         <Link to={`/Quiz?data=${encodeURIComponent(quizData)}`}>
           <button className="start text-center">Start quiz</button>
         </Link> :
